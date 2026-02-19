61ff382724327828dc98fd33a6d613d42f393547
tanstevn <tanstevenlester@gmail.com>
2026-02-20 05:49:04 +0800

    Frontend: Add codebase structure

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Helvetica", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
