f38a6e029927630a61d1763c06dd3373f721cdcf
Steven Tan <tanstevenlester@gmail.com>
2026-02-23 18:58:33 +0800

    TPY-17: Add migration files for the initial domains and seeding initial data (#3)
    * TYP-17: Initial domains
    
    * TPY-17: Add configuration for each MVP domains (not final) and uses EF Core + PostgreSQL for database connection
    
    * TPY-17: Add uniqueness for 'Email' and 'Mobile' columns/props for UserInfo; Make ApplicationDbContext use primary constructor
    
    * TPY-17: Add migration file for the initial domains and seeding initial data for Users, UserInfos, and Roles
    
    * TPY-17: Fix typo for UserInfos column 'FirstName'

---
+++ Backend/Transparity.Data/Migrations/20260223085809_SeedInitialData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Transparity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var utcNow = DateTime.UtcNow;
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: ["Id", "Name", "Description", "CreatedAt", "DeletedAt"],
+                values: [1L, "Anonymous", "Anonymous Role", utcNow, null]
+            );
+
+            migrationBuilder.InsertData(
+                table: "UserInfos",
+                columns: ["Id", "FirstName", "LastName", "Email", "Mobile", "Address1", "Address2"],
+                values: [1L, "Anonymous", "Anonymous", null, null, "Anonymous", null]
+            );
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: ["Id", "UserInfoId", "RoleId", "IsVerified", "CreatedAt", "DeletedAt"],
+                values: [1L, 1L, 1L, true, utcNow, null]
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(table: "Users", keyColumn: "Id", keyValue: 1L);
+            migrationBuilder.DeleteData(table: "UserInfos", keyColumn: "Id", keyValue: 1L);
+            migrationBuilder.DeleteData(table: "Roles", keyColumn: "Id", keyValue: 1L);
+        }
+    }
+}
