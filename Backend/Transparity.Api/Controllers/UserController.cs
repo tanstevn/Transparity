aad9771183de172cdf2a8a297bfa840f15b44f09
tanstevn <tanstevenlester@gmail.com>
2026-03-15 04:17:45 +0800

    TPY-29: Add new API controller named UserController. Add new abstraction to identify if a request is user scoped. Add Create domain methods in User and UserInfo entities. Add UserInfoRecord as DTO. Update BaseUnitTest and BaseUnitTestWithValidator to run declared abstract validators of a request. Add new command under Users feature named CreateUserCommand with default behavior and create unit tests to strictly follow TDD.

---
+++ Backend/Transparity.Api/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Transparity.Application.Abstractions;
+using Transparity.Application.Users.Commands;
+using Transparity.Shared.Models;
+
+namespace Transparity.Api.Controllers {
+    [Route("api/user")]
+    public class UserController : BaseController {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator) {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<Result<CreateUserCommandResult>> CreateUser([FromBody] CreateUserCommand command) {
+            return await _mediator.SendAsync(command);
+        }
+    }
+}
---
+++ Backend/Transparity.Shared/Enums/RoleEnums.cs
@@ -0,0 +1,7 @@
+﻿namespace Transparity.Shared.Enums {
+    public enum RoleEnums {
+        None = 0,
+        Anonymous = 1,
+        Citizen = 2
+    }
+}
